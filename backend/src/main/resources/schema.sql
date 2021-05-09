243a6f8406dd461a2678f438d8def30a454e0b0d
woody <woojihye2339@gmail.com>
2021-05-09 17:09:18 +0900

    [#2] edit : schema.sql 파일 위치 변경
    > resource 폴더 바로 아래 위치
      - resource/static/ ➡️ resource/

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -111,13 +111,40 @@ CREATE TABLE IF NOT EXISTS `baseball_db`.`inning` (
                                                       `inning_number` INT UNSIGNED NOT NULL,
                                                       `score` INT UNSIGNED NOT NULL,
                                                       `inning_half` TINYINT UNSIGNED NOT NULL,
+                                                      `base_one` INT NOT NULL,
+                                                      `base_two` INT NOT NULL,
+                                                      `base_three` INT NOT NULL,
                                                       `game_id` INT NOT NULL,
-                                                      `outs` INT UNSIGNED NOT NULL,
+                                                      `pitcher` INT NOT NULL,
                                                       PRIMARY KEY (`id`),
+    INDEX `fk_inning_player1_idx` (`base_one` ASC) VISIBLE,
+    INDEX `fk_inning_player2_idx` (`base_two` ASC) VISIBLE,
+    INDEX `fk_inning_player3_idx` (`base_three` ASC) VISIBLE,
     INDEX `fk_inning_game1_idx` (`game_id` ASC) VISIBLE,
+    INDEX `fk_inning_player4_idx` (`pitcher` ASC) VISIBLE,
+    CONSTRAINT `fk_inning_player1`
+    FOREIGN KEY (`base_one`)
+    REFERENCES `baseball_db`.`player` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_inning_player2`
+    FOREIGN KEY (`base_two`)
+    REFERENCES `baseball_db`.`player` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_inning_player3`
+    FOREIGN KEY (`base_three`)
+    REFERENCES `baseball_db`.`player` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
     CONSTRAINT `fk_inning_game1`
     FOREIGN KEY (`game_id`)
     REFERENCES `baseball_db`.`game` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_inning_player4`
+    FOREIGN KEY (`pitcher`)
+    REFERENCES `baseball_db`.`player` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
     ENGINE = InnoDB;
@@ -131,41 +158,13 @@ DROP TABLE IF EXISTS `baseball_db`.`play_history` ;
 CREATE TABLE IF NOT EXISTS `baseball_db`.`play_history` (
                                                             `id` INT NOT NULL AUTO_INCREMENT,
                                                             `batter` INT NOT NULL,
-                                                            `game_id` INT NOT NULL,
-                                                            `result` INT NOT NULL,
-                                                            `base_one` INT NULL,
-                                                            `base_two` INT NULL,
-                                                            `base_three` INT NULL,
                                                             `inning_id` INT NOT NULL,
-                                                            PRIMARY KEY (`id`),
+                                                            `result` CHAR(1) NOT NULL,
+    PRIMARY KEY (`id`),
     INDEX `fk_play_member1_idx` (`batter` ASC) VISIBLE,
-    INDEX `fk_play_history_game1_idx` (`game_id` ASC) VISIBLE,
-    INDEX `fk_play_history_player1_idx` (`base_one` ASC) VISIBLE,
-    INDEX `fk_play_history_player2_idx` (`base_two` ASC) VISIBLE,
-    INDEX `fk_play_history_player3_idx` (`base_three` ASC) VISIBLE,
     INDEX `fk_play_history_inning1_idx` (`inning_id` ASC) VISIBLE,
     CONSTRAINT `fk_play_member1`
     FOREIGN KEY (`batter`)
-    REFERENCES `baseball_db`.`player` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_play_history_game1`
-    FOREIGN KEY (`game_id`)
-    REFERENCES `baseball_db`.`game` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_play_history_player1`
-    FOREIGN KEY (`base_one`)
-    REFERENCES `baseball_db`.`player` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_play_history_player2`
-    FOREIGN KEY (`base_two`)
-    REFERENCES `baseball_db`.`player` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT `fk_play_history_player3`
-    FOREIGN KEY (`base_three`)
     REFERENCES `baseball_db`.`player` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
@@ -210,25 +209,6 @@ CREATE TABLE IF NOT EXISTS `baseball_db`.`user_selected` (
     CONSTRAINT `fk_user_prefer_team1`
     FOREIGN KEY (`team_id`)
     REFERENCES `baseball_db`.`team` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-    ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `baseball_db`.`play`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `baseball_db`.`play` ;
-
-CREATE TABLE IF NOT EXISTS `baseball_db`.`play` (
-                                                    `id` INT NOT NULL,
-                                                    `is_strike` TINYINT NOT NULL,
-                                                    `play_history_id` INT NOT NULL,
-                                                    PRIMARY KEY (`id`),
-    INDEX `fk_play_play_history2_idx` (`play_history_id` ASC) VISIBLE,
-    CONSTRAINT `fk_play_play_history2`
-    FOREIGN KEY (`play_history_id`)
-    REFERENCES `baseball_db`.`play_history` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
     ENGINE = InnoDB;
