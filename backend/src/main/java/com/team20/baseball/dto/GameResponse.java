14aed57a982e90ec480f18a39c532cc0b108e1f3
woody <woojihye2339@gmail.com>
2021-05-09 17:12:31 +0900

    [#11] feature : GameResponse 클래스 구현
    > 게임 대진표 요청에 대한 응답 래핑 클래스 구현

--- backend/src/main/java/com/team20/baseball/dto/GameResponse.java
+++ backend/src/main/java/com/team20/baseball/dto/GameResponse.java
@@ -1,4 +1,32 @@
 package com.team20.baseball.dto;
 
+import com.team20.baseball.domain.game.Game;
+import com.team20.baseball.domain.team.Team;
+
 public class GameResponse {
+    private Long gameId;
+    private Team home;
+    private Team away;
+
+    private GameResponse(Long gameId, Team home, Team away) {
+        this.gameId = gameId;
+        this.home = home;
+        this.away = away;
+    }
+
+    public Long getGameId() {
+        return gameId;
+    }
+
+    public Team getHome() {
+        return home;
+    }
+
+    public Team getAway() {
+        return away;
+    }
+
+    public static GameResponse of(Game game, Team home, Team away){
+        return new GameResponse(game.getId(), home, away);
+    }
 }
