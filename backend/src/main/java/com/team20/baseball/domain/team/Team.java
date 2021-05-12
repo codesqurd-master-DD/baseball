edd8128142e534a32aae6cbf5102de135e87ab8b
woody <woojihye2339@gmail.com>
2021-05-12 12:45:15 +0900

    [#11] feature : Team 클래스에 players 필드 추가
    > Player 테이블의 team_id PK를 참조해서 해당하는 팀에 소속된 모든 플레이어들을 불러옴

--- backend/src/main/java/com/team20/baseball/domain/team/Team.java
+++ backend/src/main/java/com/team20/baseball/domain/team/Team.java
@@ -1,12 +1,26 @@
 package com.team20.baseball.domain.team;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Table("team")
 public class Team {
+    @Id
     private Long id;
     private String name;
 
-    public Team(Long id, String name) {
+    @MappedCollection(idColumn = "team_id")
+    private Set<Player> players = new HashSet<>();
+
+    public Team(Long id, String name,Set<Player> players) {
         this.id = id;
         this.name = name;
+        this.players = players;
     }
 
     public Long getId() {
@@ -16,4 +30,20 @@ public class Team {
     public String getName() {
         return name;
     }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public Set<Player> getBatters(){
+        return players.stream()
+                .filter(player -> player.getIsPitcher())
+                .collect(Collectors.toSet());
+    }
+    public Player getPitcher(){
+        return players.stream()
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
 }
