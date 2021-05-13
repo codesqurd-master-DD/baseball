f8cdfe94d4ce87f39fc951a96a4834eadf83071e
woody <woojihye2339@gmail.com>
2021-05-13 15:50:55 +0900

    [#16] feature : StartRequest 속성 추가
    > Inning 데이터 생성에 필요한 속성 추가
      - gameId
      - pitcher

--- backend/src/main/java/com/team20/baseball/dto/StartRequest.java
+++ backend/src/main/java/com/team20/baseball/dto/StartRequest.java
@@ -1,19 +1,37 @@
 package com.team20.baseball.dto;
 
+import com.team20.baseball.domain.play.Inning;
+
 public class StartRequest {
+    private Long gameId;
     private Long inningNumber;
+    private Long pitcher;
     private boolean isTop;
 
-    public StartRequest(Long inningNumber, boolean isTop) {
+    public StartRequest(Long gameId, Long inningNumber, Long pitcher, boolean isTop) {
+        this.gameId = gameId;
         this.inningNumber = inningNumber;
+        this.pitcher = pitcher;
         this.isTop = isTop;
+    }
+
+    public Long getGameId() {
+        return gameId;
     }
 
     public Long getInningNumber() {
         return inningNumber;
     }
 
+    public Long getPitcher() {
+        return pitcher;
+    }
+
     public boolean isTop() {
         return isTop;
+    }
+
+    public Inning toInning() {
+        return new Inning(this.gameId, this.inningNumber, this.pitcher, this.isTop);
     }
 }
