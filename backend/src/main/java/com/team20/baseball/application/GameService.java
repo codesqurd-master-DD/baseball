5ebc6c6cafe4c13db42608abd341a226195bf2e5
woody <woojihye2339@gmail.com>
2021-05-14 12:23:03 +0900

    [#16] feature : playScore 초기화 로직 적용
    > GameService::initPlayScore
      - team과 gameId 데이터를 통해 초기화된 playerScore 데이터 생성 및 저장

--- backend/src/main/java/com/team20/baseball/application/GameService.java
+++ backend/src/main/java/com/team20/baseball/application/GameService.java
@@ -2,6 +2,8 @@ package com.team20.baseball.application;
 
 import com.team20.baseball.domain.game.Game;
 import com.team20.baseball.domain.game.GameRepository;
+import com.team20.baseball.domain.play.PlayerScoreRepository;
+import com.team20.baseball.domain.team.Player;
 import com.team20.baseball.domain.team.Team;
 import com.team20.baseball.domain.team.TeamRepository;
 import com.team20.baseball.dto.GameResponse;
@@ -18,10 +20,12 @@ public class GameService {
 
     private final GameRepository gameRepository;
     private final TeamRepository teamRepository;
+    private final PlayerScoreRepository playerScoreRepository;
 
-    public GameService(GameRepository gameRepository, TeamRepository teamRepository) {
+    public GameService(GameRepository gameRepository, TeamRepository teamRepository, PlayerScoreRepository playerScoreRepository) {
         this.gameRepository = gameRepository;
         this.teamRepository = teamRepository;
+        this.playerScoreRepository = playerScoreRepository;
     }
 
     public List<GameResponse> gameList() {
@@ -31,7 +35,7 @@ public class GameService {
                 .collect(Collectors.toList());
     }
 
-    public List<GameResponse> gameList(PagingRequest pagingRequest){
+    public List<GameResponse> gameList(PagingRequest pagingRequest) {
         return gameRepository.findAll(pagingRequest.toPageRequest())
                 .stream()
                 .map(game -> gameResponse(game))
@@ -45,15 +49,23 @@ public class GameService {
         return GameResponse.of(game, home, away);
     }
 
-    public MatchResponse matchDetail(Long gameId, Long teamId){
-        if (!gameRepository.isSelected(teamId)){
+    public MatchResponse matchDetail(Long gameId, Long teamId) {
+        if (!gameRepository.isSelected(teamId)) {
             Game game = gameRepository.findById(gameId).orElseThrow(IllegalArgumentException::new);
 
             Team home = teamRepository.findById(game.getHomeId()).orElseThrow(() -> new TeamNotFoundException("Could not found home team"));
             Team away = teamRepository.findById(game.getAwayId()).orElseThrow(() -> new TeamNotFoundException("Could not found home team"));
+
+            initPlayerScore(home, gameId);
+            initPlayerScore(away, gameId);
             return MatchResponse.of(home, away);
         }
         return MatchResponse.of();
+    }
+
+    private void initPlayerScore(Team team, Long gameId) {
+        team.getPlayers().stream()
+                .forEach(player -> playerScoreRepository.init(gameId, player.getId()));
     }
 
 }
