682dd90aba955d2ef5a3d5681120155c6f06a78a
woody <woojihye2339@gmail.com>
2021-05-14 13:57:31 +0900

    [#16] feature : InningRepository에 update 커스텀 쿼리 추가
    > Inning 테이블 정보 업데이트를 위한 커스텀 쿼리 작성

--- backend/src/main/java/com/team20/baseball/domain/play/InningRepository.java
+++ backend/src/main/java/com/team20/baseball/domain/play/InningRepository.java
@@ -1,7 +1,13 @@
 package com.team20.baseball.domain.play;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface InningRepository extends CrudRepository<Inning, Long> {
+
+    @Modifying
+    @Query("UPDATE inning SET score = :score, base_one = :baseOne, base_two = :baseTwo, base_three = :baseThree WHERE id = :inningId")
+    void update(Long inningId, int score, Long baseOne, Long baseTwo, Long baseThree);
 
 }
